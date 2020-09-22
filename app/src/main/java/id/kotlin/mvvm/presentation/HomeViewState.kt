a112b40e38bcb85586ce3d614b01cb322fc095bc
Feri Yusuf <feri.yusuf@compnet.co.id>
2020-09-23 00:14:41 +0700

    Init on new branch

--- app/src/main/java/id/kotlin/mvvm/presentation/HomeViewState.kt
+++ app/src/main/java/id/kotlin/mvvm/presentation/HomeViewState.kt
@@ -3,6 +3,7 @@ package id.kotlin.mvvm.presentation
 import id.kotlin.mvvm.data.HomeResponse
 
 sealed class HomeViewState {
+
     object Loading : HomeViewState()
 
     data class Success(val response: HomeResponse) : HomeViewState()
