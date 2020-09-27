5ea12124d8deca03fe4c6e5bd57d2a5a207b9cdc
Feri Yusuf <feri.yusuf@compnet.co.id>
2020-09-27 23:58:02 +0700

    Code Implementation Part 3

--- app/src/main/java/id/kotlin/mvvm/presentation/HomeAdapter.kt
+++ app/src/main/java/id/kotlin/mvvm/presentation/HomeAdapter.kt
@@ -7,39 +7,75 @@ import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import id.kotlin.mvvm.R
 import id.kotlin.mvvm.data.Result
+import id.kotlin.mvvm.domain.HomeEntity
 import id.kotlin.mvvm.presentation.HomeAdapter.HomeViewHolder
 import kotlinx.android.synthetic.main.item_home.view.*
+import java.lang.RuntimeException
 
-class HomeAdapter(private val results: List<Result>) : Adapter<HomeViewHolder>() {
+enum class Type {
+    DATA,
+    LOADING
+}
+
+class HomeAdapter(private val results: MutableList<HomeEntity.Result?>) : Adapter<ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeViewHolder {
-        return HomeViewHolder(
-            LayoutInflater
-                .from(parent.context)
-                .inflate(
-                    R.layout.item_home,
-                    parent,
-                    false
+        return when (viewType) {
+            Type.DATA.ordinal -> {
+                HomeViewHolder(
+                    LayoutInflater
+                        .from(parent.context)
+                        .inflate(
+                            R.layout.item_home,
+                            parent,
+                            false
+                        )
                 )
-        )
+            }
+            Type.LOADING.ordinal -> {
+                HomeViewHolder(
+                    LayoutInflater
+                        .from(parent.context)
+                        .inflate(
+                            R.layout.item_loading,
+                            parent,
+                            false
+                        )
+                )
+            }
+            else -> throw RuntimeException("Illegal view type")
+        }
     }
 
     override fun getItemCount(): Int {
         return results.count()
     }
 
-    override fun onBindViewHolder(holder: HomeViewHolder, position: Int) {
-        holder.bind(results[holder.adapterPosition])
+    override fun getItemViewType(position: Int): Int {
+        return when {
+            results[position] == null -> Type.LOADING.ordinal
+            else -> Type.DATA.ordinal
+        }
     }
 
-    inner class HomeViewHolder(itemView: View) : ViewHolder(itemView) {
-
-        fun bind(result: Result) {
-            with(itemView) {
-                tv_title.text = result.title
-                tv_overview.text = result.overview
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        when (holder) {
+            is HomeViewHolder -> {
+                holder.bind(results[holder.adapterPosition])
             }
         }
     }
 
+
+    inner class HomeViewHolder(itemView: View) : ViewHolder(itemView) {
+
+        fun bind(result: HomeEntity.Result?) {
+            with(itemView) {
+                tv_title.text = result?.title ?: "Untitled"
+                tv_overview.text = result?.overview ?: "No description"
+            }
+        }
+    }
+
+    inner class LoadingViewHolder(itemView: View) : ViewHolder(itemView)
 }
